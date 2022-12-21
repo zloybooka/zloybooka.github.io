fe98a908c615ae3991eb961cbcf0328c17ebf9a1
zloybooka <92056467+zloybooka@users.noreply.github.com>
2022-12-21 20:42:22 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -89,6 +89,6 @@ let usercard = document.getElementById("usercard");
 let p = document.createElement("p");
 
 p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+${tg.initDataUnsafe.user.last_name}`
 
 usercard.appendChild(p);
